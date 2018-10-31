0441551aed54ee3d0afb31cc0ac6336e622beb35
Ryan Ureta <rcureta95@gmail.com>
2018-10-31 13:38:01 -0700

    part two dispatch boogaloo

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -8,46 +8,46 @@ import {fetchMovies} from '../actions/movies';
 
 export class SearchBar extends React.Component {
 
-constructor(props) {
-        super(props);
 
-        this.state = {
-                query: '',
-    results: []
- 
-        };
-    }
+	state = {
+		query: '',
+       		results: []
 
-  handleInputChange = () => {
-    this.setState({
-      query: this.search.value
-    }, () => {
-      if (this.state.query && this.state.query.length > 1) {
-        if (this.state.query.length % 2 === 0) {
-           this.props.dispatch(fetchMovies(this.state.query));
-        }
-      } else if (!this.state.query) {
-      }
-    })
-  }
+	};
 
-  render() {
-    return (
-      <form>
-        <input
-          placeholder="Search for..."
-          ref={input => this.search = input}
-          onChange={this.handleInputChange}
-        />
-        <Suggestions results={this.state.results} />
-      </form>
-    )
-  }
+	handleInputChange (e){
+		const query = e.target.value;
+
+		this.setState({
+				query
+		});
+
+		//if (this.state.query && this.state.query.length > 1) {
+		if (this.state.query.length % 2 === 0) {
+		console.log(this.props);
+		this.props.dispatch(fetchMovies(this.state.query));
+		}
+		//} else if (!this.state.query) {
+		//}
+
+	}
+
+	render() {
+		return (
+				<form>
+				<input
+				placeholder="Search for..."
+				onChange={(e)=>this.handleInputChange(e)}
+				/>
+				<Suggestions results={this.state.results} />
+				</form>
+		       )
+	}
 }
 
-const mapStateToProps = state => {
-    return {
-   };
-};
-export default connect(mapStateToProps)(SearchBar);
+/*const mapStateToProps = state => {
+  return {
+  };
+  };*/
+export default connect()(SearchBar);
 
