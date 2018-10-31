4da56ee2425c5e83bc7a73589efa9d5682d2a7f7
Tarik Desire <tprdesire@gmail.com>
2018-10-31 11:19:42 -0400

    Rewiring config vars

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@
 require('dotenv').config();
 
 module.exports = {
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://goseethis-server.herokuapp.com/api',
   MOVIE_SEARCH_BASE_URL: `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}`
 };
