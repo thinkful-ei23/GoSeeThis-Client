8456f0e1e32230f49d5e6ba96a15d6f8b190128e
David Folks <aldwyn.wot@gmail.com>
2018-11-01 12:33:31 -0600

    updated my-recommendation.js

--- src/components/my-recommended.js
+++ src/components/my-recommended.js
@@ -5,20 +5,22 @@ import { connect } from 'react-redux';
 import './my-recommended.css';
 
 export class MyRecommended extends React.Component {
-  componentDidMount() {
-    this.props.dispatch(fetchUserRecs('5bdb25eb0461975788756cf2'));
+  componentDidUpdate() {
+    if (this.props.user) {
+      return this.props.dispatch(fetchUserRecs(this.props.user.id));
+    }
   }
 
   render() {
     let recs;
+    let username;
     if (this.props.recs) {
-      console.log(this.props.recs);
-      const recs = this.props.recs.map((rec, index) => {
+      recs = this.props.recs.map((rec, index) => {
         return (
           <li key={index}>
             <section className="recommended">
               <section className="movie-title">
-                <img src={rec.posterUrl} alt="movie poster" />
+                <img src={rec.posterUrl} alt="movie poster" width="75px" />
                 <h3>{rec.title}</h3>
               </section>
               <section className="recommend-desc">
@@ -28,13 +30,14 @@ export class MyRecommended extends React.Component {
           </li>
         );
       });
-      return recs;
     }
-
+    if (this.props.user) {
+      username = this.props.user.username;
+    }
     return (
       <section className="myRecommended">
         <section className="username">
-          <h2>Graves</h2>
+          <h2>{username}</h2>
         </section>
         <section className="recommended-list">
           <section className="recomendation-header">
@@ -48,9 +51,8 @@ export class MyRecommended extends React.Component {
 }
 
 const mapStateToProps = state => ({
-  recs: state.recs.userRecs
-  //   userId: state.auth.currentUser.id
-  //   username: state.auth.currentUser.username
+  recs: state.recs.userRecs,
+  user: state.auth.currentUser
 });
 
 export default connect(mapStateToProps)(MyRecommended);
