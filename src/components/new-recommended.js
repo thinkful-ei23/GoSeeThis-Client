2c8923882a3b769f8e78a3082108738dc099ba1f
Ryan Ureta <rcureta95@gmail.com>
2018-11-02 11:19:08 -0700

    ready to test

--- src/components/new-recommended.js
+++ src/components/new-recommended.js
@@ -4,6 +4,7 @@ import {Field, reduxForm, focus} from 'redux-form';
 import Input from './input';
 import ReccomendTitleInput from './reccomendtitleinput';
 import {createRec} from '../actions/recommendations';
+import {required} from './stupidvalidator';
 import './new-recommended.css';
 
 export class NewRecommended extends React.Component{
@@ -15,6 +16,8 @@ export class NewRecommended extends React.Component{
             searchResultTitle: '',
         }
     }
+
+
     onSubmit(values){
         console.log(values);
         const recDesc = values.description;
@@ -37,21 +40,23 @@ export class NewRecommended extends React.Component{
     }
 
     render(){
-        return(
-            <form
+
+	return(
+
+	    <form
             className="login-form" onSubmit = {this.props.handleSubmit(values => this.onSubmit(values))}>
             <legend className="recommend-title">New Recommendation</legend>
             <label htmlFor="title">Movie Title</label>
             <ReccomendTitleInput />
             <label htmlFor="description">Why Recommended</label>
-            <Field component={Input} type="text" name="description" required />
+            <Field component={Input} type="text" name="description" validate={[required]} />
             <button
                 type="submit">
                 Create
             </button>
             
         </form>
-        )
+        );
     }
 }
 
