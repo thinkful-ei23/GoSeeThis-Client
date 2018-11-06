809df0c432c517279342deeaaa856f7805f9f4ef
David Folks <aldwyn.wot@gmail.com>
2018-11-06 09:01:01 -0700

    fixed a typo

--- src/components/reccomendtitleinput.js
+++ src/components/reccomendtitleinput.js
@@ -5,31 +5,31 @@ import { connect } from 'react-redux';
 import { fetchMovies } from '../actions/movies';
 import ReccomendTitleSuggestions from './reccomendtitlesuggestions';
 import './reccomendtitleinput.css';
-import {deleteRecMovie} from '../actions/movies';
+import { deleteRecMovie } from '../actions/movies';
 export class ReccomendTitleInput extends React.Component {
+  //add state
+  state = {
+    query: '',
+    searchResultTitle: '',
+    currentSelection: 'No Movie Title Selected'
+  };
 
-	//add state
-	state = {
-	    query: '',
-	    searchResultTitle: '',
-	    currentSelection: 'No Movie Title Selected'
-	}
-
-	handleInputChange(e) {
-	this.props.dispatch(deleteRecMovie());
-	 const query = e.target.value;
-   	 this.setState({query, searchResultTitle:e.target.value});
-   	 if (query) {
-   	 this.props.dispatch(fetchMovies(query));
-         }
-	}
+  handleInputChange(e) {
+    this.props.dispatch(deleteRecMovie());
+    const query = e.target.value;
+    this.setState({ query, searchResultTitle: e.target.value });
+    if (query) {
+      this.props.dispatch(fetchMovies(query));
+    }
+  }
 
   render() {
     if (!this.state.query) {
       return (
-=
         <div>
-          <span className='displayOfCurrentRecTitleSelection'>Currently Selected Title: None Currently Selected</span>
+          <span className="displayOfCurrentRecTitleSelection">
+            Currently Selected Title: None Currently Selected
+          </span>
           <input
             placeholder="Search for..."
             onChange={e => this.handleInputChange(e)}
@@ -41,7 +41,9 @@ export class ReccomendTitleInput extends React.Component {
     if (!this.props.searchResults && !this.props.loading) {
       return (
         <div>
-          <span className='displayOfCurrentRecTitleSelection'>Currently Selected Title: None Currently Selected</span>
+          <span className="displayOfCurrentRecTitleSelection">
+            Currently Selected Title: None Currently Selected
+          </span>
           <input
             placeholder="Search for..."
             onChange={e => this.handleInputChange(e)}
@@ -55,36 +57,35 @@ export class ReccomendTitleInput extends React.Component {
             placeholder="Search for..."
             onChange={e => this.handleInputChange(e)}
           />
-          <ReccomendTitleSuggestions results='Loading...' />
+          <ReccomendTitleSuggestions results="Loading..." />
         </div>
-      )
-    }
-     else if(this.props.recMovieData) {
-   return (
-        <div>
-         <span className='displayOfCurrentRecTitleSelection'>Currently Selected Title: {this.props.recMovieData.title}</span>
-	<input
-          placeholder="Search for..."
-          onChange={(e)=>this.handleInputChange(e)}
-          value = {this.props.recMovieData.title}
-	  />
-        </div>
-      )
-    
-    }
-
-    else {
+      );
+    } else if (this.props.recMovieData) {
       return (
-	<div>
-         <span className='displayOfCurrentRecTitleSelection'>Currently Selected Title: None Currently Selected</span>
+        <div>
+          <span className="displayOfCurrentRecTitleSelection">
+            Currently Selected Title: {this.props.recMovieData.title}
+          </span>
           <input
-          placeholder="Search for..."
-          onChange={(e)=>this.handleInputChange(e)}
+            placeholder="Search for..."
+            onChange={e => this.handleInputChange(e)}
+            value={this.props.recMovieData.title}
           />
-	 <ReccomendTitleSuggestions results={this.props.searchResults} />
         </div>
-      )
-
+      );
+    } else {
+      return (
+        <div>
+          <span className="displayOfCurrentRecTitleSelection">
+            Currently Selected Title: None Currently Selected
+          </span>
+          <input
+            placeholder="Search for..."
+            onChange={e => this.handleInputChange(e)}
+          />
+          <ReccomendTitleSuggestions results={this.props.searchResults} />
+        </div>
+      );
     }
   }
 }
